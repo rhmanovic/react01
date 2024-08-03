12e4132be75b4d25df86a8a6bc78fc4858c8f239
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-08-03 19:03:47 +0300

    Create Page1.jsx

---
+++ src/Page1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// https://github.com/rhmanovic/react01.git
+
+const Page1 = () => {
+    return (
+        <div>
+            <h1>Page 1</h1>
+            <p>This is the content of Page 1.</p>
+        </div>
+    );
+};
+
+export default Page1;
